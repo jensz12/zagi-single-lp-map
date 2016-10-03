a1c288253caba84c04ada1814756dbcdfc9883cd
Jens Møller <jens@jensz12.com>
2016-10-03 14:37:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,13 @@ def billedeFilter(poi):
         except KeyError:
             return poi['name'] + '\n'
 
+def portalFilter(poi):
+    if poi['id'] == 'Portaler':
+        try:
+            return (poi['name'], poi['description'])
+        except KeyError:
+            return poi['name'] + '\n'
+
 end_smooth_lighting = [Base(), EdgeLines(), SmoothLighting(strength=0.5)]
 
 # Define the path to your world here. 'Zagi Single Player' in this case will show up as
@@ -54,11 +61,49 @@ renders["singleoverworld"] = {
                     'y':64,
                     'z':-910,
                     'name':'Billedet af Zagi',
-                    'description':'Billedet af Zagi, påbegyndt i episode 90, færdigjort i episode ??'},],
+                    'description':'Billedet af Zagi, påbegyndt i episode 90, færdigjort i episode ??'},
+                    {'id':'Portaler',
+                    'x':2920,
+                    'y':64,
+                    'z':2300,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':2762,
+                    'y':64,
+                    'z':3856,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':10,
+                    'y':64,
+                    'z':671,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-1793,
+                    'y':64,
+                    'z':881,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-3270,
+                    'y':64,
+                    'z':795,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-2906,
+                    'y':64,
+                    'z':-1335,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    ],
 	'markers':[ 
 				dict(name="FarmSheep", filterFunction=farmSheepFilter, icon="icons/marker_farm_sheep.png"),
                 dict(name="Skib", filterFunction=shipFilter, icon="icons/marker_ship.png"),
-                dict(name="Billede", filterFunction=billedeFilter, icon="icons/marker_zagi.png"),],
+                dict(name="Billede", filterFunction=billedeFilter, icon="icons/marker_zagi.png"),
+                dict(name="Portaler", filterFunction=portalFilter, icon="icons/marker_portal.png"),],
 }
 # Overworld caves
 renders["singlecave"] = {
@@ -91,11 +136,48 @@ renders["singlenight"] = {
                     'y':64,
                     'z':-910,
                     'name':'Billedet af Zagi',
-                    'description':'Billedet af Zagi, påbegyndt i episode 90, færdigjort i episode ??'},],
+                    'description':'Billedet af Zagi, påbegyndt i episode 90, færdigjort i episode ??'},
+                    {'id':'Portaler',
+                    'x':2920,
+                    'y':64,
+                    'z':2300,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':2762,
+                    'y':64,
+                    'z':3856,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':10,
+                    'y':64,
+                    'z':671,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-1793,
+                    'y':64,
+                    'z':881,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-3270,
+                    'y':64,
+                    'z':795,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},
+                    {'id':'Portaler',
+                    'x':-2906,
+                    'y':64,
+                    'z':-1335,
+                    'name':'Portal',
+                    'description':'Portal til Nether'},],
 	'markers':[ 
 				dict(name="FarmSheep", filterFunction=farmSheepFilter, icon="icons/marker_farm_sheep.png"),
                 dict(name="Skib", filterFunction=shipFilter, icon="icons/marker_ship.png"),
-                dict(name="Billede", filterFunction=billedeFilter, icon="icons/marker_zagi.png")],
+                dict(name="Billede", filterFunction=billedeFilter, icon="icons/marker_zagi.png"),
+                dict(name="Portaler", filterFunction=portalFilter, icon="icons/marker_portal.png"),],
 }
 # Nether med Smooth Lightning
 renders["singlenether"] = {
